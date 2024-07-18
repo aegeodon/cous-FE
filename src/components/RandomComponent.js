9169916b81f1f47bc2222b2954c8cf0b859ec527
letthem <suyeon3662@naver.com>
2024-07-18 19:55:32 +0900

    random UI

--- src/components/RandomComponent.js
+++ src/components/RandomComponent.js
@@ -1,3 +1,27 @@
-export function RandomComponent() {
-  return <></>;
-}
+import styled from "styled-components";
+import random1 from "./../assets/Home/random1.svg";
+import randomBtn from "./../assets/Home/randomBtn.svg";
+const RandomComponent = () => {
+  return (
+    <Div>
+      <img src={random1} width={"632px"} />
+      <Img>
+        <img src={randomBtn} width={"190px"} />
+      </Img>
+    </Div>
+  );
+};
+
+export default RandomComponent;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Img = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
