ea69c76d9898c88bd46dc57cf1e73b1b48459ef5
jeongin <saon0410@gmail.com>
2024-07-18 16:37:27 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./reset.css";
-import Home from "./pages/Home.js";
-import StartPage from "./pages/StartPage.js";
-import NotFoundPage from "./pages/NotFoundPage.js";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './reset.css';
+import Home from './pages/Home.js';
+import StartPage from './pages/StartPage.js';
+import NotFoundPage from './pages/NotFoundPage.js';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/start" element={<StartPage />} />
-          <Route path="*" element={<NotFoundPage/>}/>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,7 +1,70 @@
+import KakaoLogin from "../components/KakaoLogin";
+import styled from "styled-components";
+
+const StartWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`;
+const GrayDiv = styled.div`
+  width: 1200px;
+  height: 600px;
+  margin: 150px 40px 80px;
+  flex-shrink: 0;
+  border-radius: 84px;
+  padding: 10px;
+  background: #f1f1f1;
+`;
+
+const StyleDiv = styled.div`
+  p {
+    color: #000;
+    text-align: center;
+    font-family: "SUIT";
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+
+  span {
+    font-weight: 600;
+  }
+`;
+
+const CourseButton = styled.button`
+  display: inline-flex;
+  padding: 21.5px 43.5px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  background: #000;
+  p {
+    color: white;
+  }
+`;
+
 const StartPage = () => {
   return (
-    <>StartPage</>
-  )
-}
+    <StartWrapper>
+      <GrayDiv>
+        <StyleDiv>
+          <p>멀리 떠나야 할 것만 같은 여행</p>
+          <p>
+            일상 속 서울을 <span>새로운 여행의 공간으로</span>
+          </p>
+          <KakaoLogin />
+        </StyleDiv>
+      </GrayDiv>
+      <CourseButton>
+        <p className="logo">코럼버스</p>
+        <p>추천받기</p>
+      </CourseButton>
+    </StartWrapper>
+  );
+};
 
 export default StartPage;
